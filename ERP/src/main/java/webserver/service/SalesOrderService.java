b576130a826662e8cb081f678c8c7e201931da27
yuchenzhang <2252754@tongji.edu.cn>
2025-07-25 23:25:26 +0800

    获取销售订单详情

--- ERP/src/main/java/webserver/service/SalesOrderService.java
+++ ERP/src/main/java/webserver/service/SalesOrderService.java
@@ -2,10 +2,13 @@ package webserver.service;
 
 
 import webserver.common.Response;
+import webserver.pojo.SalesOrderDetailDTO;
 import webserver.pojo.SalesOrderSearchRequest;
 
 public interface SalesOrderService {
     Response searchSalesOrders(SalesOrderSearchRequest request);
+    Response<SalesOrderDetailDTO> getSalesOrderDetails(String soId);
+
 }
 
 
